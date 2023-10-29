c2ab44cb85fefa7d653620ba3b50f6a8273b6d44
Ibrahim Maali <maaliibrahim2022@gmail.com>
2023-10-29 23:37:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Footer from './componentns/Footer/Footer.jsx'
+import Home from './componentns/Home/Home.jsx'
+import Navbar from './componentns/Navbar/Navbar.jsx'
+import Products from './componentns/Products/Products.jsx'
+
+
+function App() {
+
+  return (
+    <>
+ <Navbar/>
+ <Products/>
+ <Home/>
+<Footer/>
+ </>
+  )
+}
+
+export default App
